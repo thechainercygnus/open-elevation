c572b9b065f457e9eee155530e101c8f93251e01
Bryce Jenkins <bryce@brycejenkins.net>
2023-04-02 17:23:09 -0600

    Again

--- create-dataset.sh
+++ create-dataset.sh
@@ -11,6 +11,12 @@ set -eu
 
 cd $OUTDIR
 ../download-srtm-data.sh
+echo 'Moving NE_250m'
+mv ./SRTM_NE_250m_TIF/SRTM_NE_250m.tif SRTM_NE_250m.tif
+echo 'Moving SE_250m'
+mv ./SRTM_NE_250m_TIF/SRTM_SE_250m.tif SRTM_SE_250m.tif
+echo 'Moving W_250m'
+mv ./SRTM_NE_250m_TIF/SRTM_W_250m.tif SRTM_W_250m.tif
 ../create-tiles.sh ./SRTM_NE_250m_TIF/SRTM_NE_250m.tif 10 10
 ../create-tiles.sh ./SRTM_SE_250m_TIF/SRTM_SE_250m.tif 10 10
 ../create-tiles.sh ./SRTM_W_250m_TIF/SRTM_W_250m.tif 10 20
